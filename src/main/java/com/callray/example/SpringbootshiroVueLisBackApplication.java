955a19f4fd9f73b066796ba74ed256a3fb969ef3
callray <myqq@qq.com>
2020-09-04 14:58:38 +0800

    修改启动类位置

--- src/main/java/com/callray/example/SpringbootshiroVueLisBackApplication.java
+++ src/main/java/com/callray/example/SpringbootshiroVueLisBackApplication.java
@@ -1,4 +1,4 @@
-package com.callray.example.springbootshirovuelisback;
+package com.callray.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
