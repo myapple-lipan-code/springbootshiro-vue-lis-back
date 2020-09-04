e08072bc68474457331760b5051d09d6c3432367
callray <myqq@qq.com>
2020-09-04 14:56:57 +0800

    初次提交权限控制系统前后端分离系统和数据库文件

---
+++ src/main/java/com/callray/example/springbootshirovuelisback/SpringbootshiroVueLisBackApplication.java
@@ -0,0 +1,13 @@
+package com.callray.example.springbootshirovuelisback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootshiroVueLisBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootshiroVueLisBackApplication.class, args);
+    }
+
+}
